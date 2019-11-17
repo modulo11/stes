a22e69142cd0fce9d030b7f77ddf2711e81ef964
Johannes Dillmann <johannes.dillmann@retest.de>
2019-11-17 15:39:35 +0100

    First commit

---
+++ src/main/java/com/github/modullo11/stes/StesApplication.java
@@ -0,0 +1,13 @@
+package com.github.modullo11.stes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/modullo11/stes/StesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.modullo11.stes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
